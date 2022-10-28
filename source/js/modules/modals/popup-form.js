428da57ff3e965b1a1e4cdcbca740e28d8da4a57
Anastasia1ukina <lukina02anastasiy@gmail.com>
2022-10-28 15:56:26 +0300

    Часть исправлена

--- source/js/modules/modals/popup-form.js
+++ source/js/modules/modals/popup-form.js
@@ -7,6 +7,7 @@ const nameInput = document.querySelector('#name-call');
 if (popupForm) {
   orderButton.addEventListener('click', (evt) => {
     evt.preventDefault();
+    pageBody.style.position = 'fixed';
     popupForm.classList.toggle('popup-form--open');
     pageBody.classList.toggle('body--opened-modal');
     nameInput.focus();
@@ -18,16 +19,20 @@ if (popupForm) {
   document.addEventListener('keydown', (evt) => {
     if (evt.key === 'Escape') {
       if (popupForm.classList.contains('popup-form--open')) {
+        pageBody.style.position = '';
         popupForm.classList.remove('popup-form--open');
       }
       if (pageBody.classList.contains('body--opened-modal')) {
+        pageBody.style.position = '';
         pageBody.classList.remove('body--opened-modal');
       }
     }
   });
   popupForm.addEventListener('click', (evt) => {
     const target = evt.target;
+    pageBody.style.position = '';
     if (!target.closest('.popup-form__wrapper')) {
+      pageBody.style.position = '';
       popupForm.classList.remove('popup-form--open');
       pageBody.classList.remove('body--opened-modal');
     }
